cea11b8af79dfd3e34e42ccd2f3d395ebb07cc03
Maxwell Matthis <contact@maxwellmatthis.de>
2023-04-08 17:53:43 +0200

    Update tests

--- tests/utils.ts
+++ tests/utils.ts
@@ -10,10 +10,10 @@ function eq(output: any, target: any) {
 
 // protocol removal
 eq(trimURL('http://localhost:8080'), 'localhost:8080');
-eq(trimURL('wss://localhost:8080'), 'localhost:8080');
-eq(trimURL('abcdefghijkl://localhost:8080'), 'localhost:8080');
+eq(trimURL('wss://localhost:2345'), 'localhost:2345');
+eq(trimURL('abcdefghijkl://localhost:4632'), 'localhost:4632');
 
 // trailing slash removal
 eq(trimURL('http://localhost:8080/'), 'localhost:8080');
-eq(trimURL('http://localhost:8080/game'), 'localhost:8080/game');
-eq(trimURL('http://localhost:8080/game/'), 'localhost:8080/game');
+eq(trimURL('http://localhost:3246/game'), 'localhost:3246/game');
+eq(trimURL('http://localhost:2345/game/'), 'localhost:2345/game');
